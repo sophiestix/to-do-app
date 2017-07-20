bdbaf01f31a04e4eb299bfded474e7858c860091
Zsofia <zsofia.helmeczi@gmail.com>
2017-07-20 14:45:35 +0200

    What are services?

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,29 @@
 angular.module("todoListApp", [])
 
-.controller('mainCtrl', function($scope) {
-  $scope.learningNgChange = function() {
-    console.log("An input changed!");
-  };
+.controller('mainCtrl', function($scope, dataService) {
 
-  $scope.todos = [
-  	{"name": "clean the house"},
-  	{"name": "water the dog"},
-  	{"name": "feed the lawn"},
-  	{"name": "pay dem bills"},
-  	{"name": "run"},
-  	{"name": "swim"},
-  ]
+	$scope.helloConsole = dataService.helloConsole;
+	
+	$scope.learningNgChange = function() {
+    	console.log("An input changed!");
+	};
 
+	$scope.todos = [
+		{"name": "clean the house"},
+		{"name": "water the dog"},
+		{"name": "feed the lawn"},
+		{"name": "pay dem bills"},
+		{"name": "run"},
+		{"name": "swim"},
+	]
+
+
+})
+
+.service('dataService',function() {
+
+	this.helloConsole = function() {
+		console.log('This is the hello console service!');
+	}
 
 });
